56e40be8385879cc821690e17451e450e122d05a
Apolo07206 <ferneycontreras0720@gmail.com>
2026-08-30 21:07:50 -0500

    estructura base

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask , render_template , request, url_for , flash ,redirect
+from flask_mysqldb import MySQL
+
+
+app=Flask(__name__)
+mysql=MySQL(app)
+
+
+@app.route('/')
+def home():
+    return 'hola mundo '
+
+if __name__ == '__main__':
+    app.run(debug=True)
