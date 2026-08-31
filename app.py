11ad285034e79383e983a454af6b4b0bf6fa205b
Apolo07206 <ferneycontreras0720@gmail.com>
2026-08-31 09:18:25 -0500

    ajustes en las conexiones a la DB

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask , render_template , request, url_for , flash ,redirect
 from flask_mysqldb import MySQL
+from config import Config
 
 
 app=Flask(__name__)
+app.config.from_object(Config)
 mysql=MySQL(app)
 
 
 app.secret_key='eicwiemcumaehfppiwgqkvr'
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] =''
+app.config['MYSQL_DB'] ='proyecto_cine'
+app.config['MYSQL_UNIX_SOCKET'] = '/opt/lampp/var/mysql/mysql.sock'
+
+
 @app.route('/')
 def home():
     return 'hola mundo '
--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # carga las variables del archivo .env
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('eicwiemcumaehfppiwgqkvr')
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
     MYSQL_HOST = os.getenv('MYSQL_HOST')
-    MYSQL_USER = os.getenv('root')
-    MYSQL_PASSWORD = os.getenv('')
-    MYSQL_DB = os.getenv('proyecto_cine')
-    MYSQL_CURSORCLASS = 'DictCursor'  # para que los resultados vengan como {clave: valor} en vez de tuplas
+    MYSQL_USER = os.getenv('MYSQL_USER')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+    MYSQL_DB = os.getenv('MYSQL_DB')
+    MYSQL_CURSORCLASS = 'DictCursor'
