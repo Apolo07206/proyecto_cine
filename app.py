91a4943ca2b3a4a712a05b8a7390032d3429b747
Apolo07206 <ferneycontreras0720@gmail.com>
2026-08-31 10:19:25 -0500

    rutas de trabajo definidas

--- app.py
+++ app.py
@@ -18,9 +18,63 @@ app.config['MYSQL_DB'] ='proyecto_cine'
 app.config['MYSQL_UNIX_SOCKET'] = '/opt/lampp/var/mysql/mysql.sock'
 
 
+# --- MÓDULO CLIENTE ---
+
 @app.route('/')
-def home():
-    return 'hola mundo '
+def inicio():
+    return render_template('home.html')
+
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/pelicula')
+def detalle_pelicula():
+    return render_template('detalle_pelicula.html')
+
+@app.route('/boletas')
+def seleccion_boletas():
+    return render_template('seleccion_boletas.html')
+
+@app.route('/mapa')
+def mapa_silla():
+    return render_template('mapa_silla.html')
+
+@app.route('/pago')
+def resumen_pago():
+    return render_template('resumen_pago.html')
+
+@app.route('/confirmacion')
+def confirmacion():
+    return render_template('confirmacion.html')
+
+
+# --- MÓDULO ADMIN ---
+
+@app.route('/admin')
+def admin_dashboard():
+    return render_template('admin/dashboard.html')
+
+@app.route('/admin/peliculas')
+def admin_peliculas():
+    return render_template('admin/peliculas.html')
+
+@app.route('/admin/salas')
+def admin_salas():
+    return render_template('admin/salas.html')
+
+@app.route('/admin/funciones')
+def admin_funciones():
+    return render_template('admin/funciones.html')
+
+@app.route('/admin/reportes')
+def admin_reportes():
+    return render_template('admin/reportes.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
